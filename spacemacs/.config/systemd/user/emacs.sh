1e1bed4ba3d2d4f26056b31b30f41178c01090fc
Patrick Lenihan <patrick@plenihan.com>
2025-04-22 18:16:04 +0100

    Wait for graphical session

--- spacemacs/.config/systemd/user/emacs.sh
+++ spacemacs/.config/systemd/user/emacs.sh
@@ -1,6 +1,17 @@
 set -e
+
 source ~/.cargo/env
+
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - bash)"
+
 export PATH=$(nvm which 22.12.0 | xargs dirname):$PATH
+export PATH="$HOME/go/bin:$PATH"
+
+DISPLAY=':0'
+
 exec /usr/local/bin/emacs --fg-daemon
